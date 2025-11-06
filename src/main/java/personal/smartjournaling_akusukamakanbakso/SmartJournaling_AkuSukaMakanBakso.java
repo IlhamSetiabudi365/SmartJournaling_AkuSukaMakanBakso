fb008f43dc1328f776149e2cf1f0a10e1668cb81
IlhamSetiabudi365 <24204166@siswa.um.edu.my>
2025-11-06 19:29:22 +0800

    push1
    hello

---
+++ src/main/java/personal/smartjournaling_akusukamakanbakso/SmartJournaling_AkuSukaMakanBakso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package personal.smartjournaling_akusukamakanbakso;
+
+/**
+ *
+ * @author Ilham365
+ */
+public class SmartJournaling_AkuSukaMakanBakso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
